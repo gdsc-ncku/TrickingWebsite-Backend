9d74b8c704ad7a7c5ebd3fe6f521321c4bc66adc
YU-WEI,HSU <blackfire106.1231@gmail.com>
2025-05-11 02:25:26 +0800

    Fix bug cause by beanie
    According to Beanie's documentation, find_all is equivalent
     to find({}).
    Using find(some_conditions, fetch_links=True) should
    automatically expand the links.
    However, the actual result does not meet expectations
    (the links are not expanded).
    Upon checking the related GitHub issue, it was found that
     this problem has been raised since 2024 but has not been
    fixed.
    Therefore, this modification is made.
    
    https://beanie-odm.dev/tutorial/finding-documents/
    https://beanie-odm.dev/tutorial/relations/
    https://github.com/BeanieODM/beanie/issues/1011

--- api/routers/tricktionary.py
+++ api/routers/tricktionary.py
@@ -6,7 +6,16 @@ router = APIRouter()
 
 @router.get("/get_all_trick")
 async def get_all_trick():
-    trick_list = await Trick.find_all(fetch_links=True).to_list()
+    trick_list = await Trick.find_all().to_list()
+
+    # I need to do this workaround because fetch_links=True doesn't 
+    # work like it should with documents. 
+    # This problem has existed for a long time. 
+    # https://github.com/BeanieODM/beanie/issues/1011
+    for trick in trick_list:
+        await trick.fetch_link("difficulty_id")
+        await trick.fetch_link("categories_id")
+
     return trick_list
 
 @router.get("/get_proficiency")
